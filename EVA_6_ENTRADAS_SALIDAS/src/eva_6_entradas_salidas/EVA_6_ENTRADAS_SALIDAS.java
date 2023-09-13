7078c3a0c7fcee29540a01bebaf45cf5cbf66acc
Tashiro13 <143568865+Tashiro13@users.noreply.github.com>
2023-09-12 19:16:18 -0600

    Add files via upload

---
+++ EVA_6_ENTRADAS_SALIDAS/src/eva_6_entradas_salidas/EVA_6_ENTRADAS_SALIDAS.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva_6_entradas_salidas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_6_ENTRADAS_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+             String marca;
+             String modelo;
+             int anio;
+             double precio;
+             //HAy un paquete System. dentro
+             System.out.println("Introduce la marca del auto");
+             System.out.println("Introdue el modelo del auto");
+             System.out.println("Introdue el año del auto");
+             System.out.println("Introdue el precio del auto");
+             
+             
+             
+              
+             
+
+  
+    }
+   
+
+}
