37600317d10b8627779bab40002a90f09c1cba8a
Tashiro13 <143568865+Tashiro13@users.noreply.github.com>
2023-09-19 19:50:50 -0600

    Add files via upload

---
+++ EVA1_11_INICIALIZACION/src/eva1_11_inicializacion/EVA1_11_INICIALIZACION.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_11_inicializacion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_11_INICIALIZACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //los parentesis denotan una función
+        //println() <----- funcion
+        //main() <---- funcion
+        System.out.println("Hola mundo");
+        int valor = 0; //Inicializar la variable para usarla
+        System.out.println(valor);
+        
+        //declaramos e inicializamo 3 doubles
+        double salario = 0, iva = 0.16 , subtotal = 1000;
+        
+        final int califa_pasar = 70;
+        final String Escuela = "instituto tecnoligico de chihuahuha II";
+        System.out.println(Escuela);
+        System.out.println("La minima para pasar es:");
+        System.out.println(califa_pasar);
+        /*
+        ignora 
+        varias lineas 
+        de texto si vana  tirar mucho
+        */
+        int x;// esto es ignorado
+        
+        
+        byte valores = 120;
+        //valores = 128:
+        int miValor = Integer.MAX_VALUE;
+        System.out.println(miValor + 1);
+        
+        
+        
+        //error: solo se pude asignar una vez
+        //califa_pasar = 100;
+        //califa_pasar = 20;
+        
+        
+      
+    
+    }
